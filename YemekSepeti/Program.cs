46ef178b0939cdb40f8f97ef99864bbbf2cdb60d
Oguzhan Colak <oguzhancolak@users.noreply.github.com>
2017-01-28 11:58:14 +0300

    Inıtial Commit

--- YemekSepeti/Program.cs
+++ YemekSepeti/Program.cs
@@ -18,36 +18,33 @@ namespace YemekSepeti
         {
             // burası hazırda verilecek bilgi
             List<int> array = new List<int>(); 
-            List<int> leftList = new List<int>();
             array.Add(1);
             array.Add(5);
             array.Add(-3);
             array.Add(7);
-            array.Add(-1);
+            array.Add(-2);
             
             int result = -1; // sonuç değeri
             int listTotalValue = array.Sum(); // liste toplam değeri
+            int leftListTotalValue = 0; // sol liste toplam değeri
 
             // Sol liste 0 elemanlı olacağından 1'den başlatıyoruz.
             for (int i = 1; i < array.Count; i++)
             {
-                // Sol listeye ekleniyor.
-                leftList.Add(array[i - 1]);
+                // Sol listeye toplamına ekleniyor.
+                leftListTotalValue += array[i - 1];
 
                 // Ara index değerinden liste toplamı çıkarılıyor.
                 int comparisonValue = listTotalValue - array[i];
 
                 // Eğer ara değer ikiye bölünemiyorsa zaten eşit olamaz.
                 if (comparisonValue % 2 == 0) {
-                    // Sol liste toplamı bulunuyor.
-                    int leftListTotalValue = leftList.Sum();
-
                     // Ara index değerinden çıkarılmış toplamdan, sol liste toplamı çıkarılıyor.
-                    // Bu bize sağ liste toplamını verecektir.
                     int remainingValue = comparisonValue - leftListTotalValue;
 
                     // Eğer sol liste toplamıyla, 
-                    // sağ liste toplamı eşitse değerimiz budur.
+                    // index ve sol liste toplamından çıkarılmış değer
+                    // bize sağ liste toplamını verir.
                     if (leftListTotalValue == remainingValue)
                     {
                         result = i;
